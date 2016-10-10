bfd5b1f2f738290eeb60360c2cf72d557b188925
frontainer <frontainer@gmail.com>
2016-10-10 10:19:23 +0900

    設定を差分だけに限定

--- config/style.config.production.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const conf = require('./style.config');
-const merge = require('webpack-merge');
-module.exports = merge(conf,{
-    sourceMap: false,
-    plugins: [  // postcssプラグイン
-        require('postcss-csso')()
-    ]
-});
