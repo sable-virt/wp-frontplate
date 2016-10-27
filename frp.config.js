c10530449dd32270af182c212f7a6cb62d833502
frontainer <frontainer@gmail.com>
2016-10-27 21:56:07 +0900

    update frp.config.js

--- frp.config.js
+++ frp.config.js
@@ -1,9 +1,17 @@
 'use strict';
-module.exports = function(production) {
-    global.FRP_DEST = 'wp';
-    return {
-        server: {
-          proxy: 'localhost'
-        }
-    }
+// https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
+module.exports = function (production) {
+  global.FRP_DEST = 'wp/static';
+  return {
+    clean: {},
+    html: {},
+    style: production ? {} : {},
+    script: production ? {} : {},
+    server: {
+      proxy: 'localhost'
+    },
+    copy: {},
+    sprite: {},
+    test: {}
+  }
 };
